3d94fabb6dc1bb7e842d000149c5151a9e289d5b
Diego OJ <diego.ojeda@binbash.com.ar>
2020-10-21 12:05:51 -0300

    Modify module to make it compatible with Terraform 12 & 13

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "monitor" {
-  source = "modules/monitor"
+  source = "./modules/monitor"
 
-  name                   = "${var.monitor_name}"
-  role_name              = "${var.monitor_role_name}"
-  slack_notification_url = "${var.monitor_slack_notification_url}"
+  name                   = var.monitor_name
+  role_name              = var.monitor_role_name
+  slack_notification_url = var.monitor_slack_notification_url
 }
 
 resource "aws_cloudwatch_event_rule" "main" {
-  name = "${var.event_rule_name}"
+  name = var.event_rule_name
 
   event_pattern = <<EOF
 {
@@ -22,13 +22,13 @@ EOF
 }
 
 resource "aws_cloudwatch_event_target" "main" {
-  rule = "${aws_cloudwatch_event_rule.main.name}"
-  arn  = "${module.monitor.arn}"
+  rule = aws_cloudwatch_event_rule.main.name
+  arn  = module.monitor.arn
 }
 
 resource "aws_lambda_permission" "main" {
   action        = "lambda:InvokeFunction"
-  function_name = "${module.monitor.arn}"
+  function_name = module.monitor.arn
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.main.arn}"
+  source_arn    = aws_cloudwatch_event_rule.main.arn
 }
